e9f6c6fe700c81d14a79c2755d841c78d8f17fe3
YusukeKokubo <yusuke.kokubo@gmail.com>
2024-03-08 10:04:05 +0900

    ncu

--- src/update_issues.ts
+++ src/update_issues.ts
@@ -1,5 +1,9 @@
 import { Client } from "@notionhq/client"
-import { NumberPropertyValue, SelectPropertyValue, TitlePropertyValue } from "@notionhq/client/build/src/api-types"
+import {
+  NumberPropertyValue,
+  SelectPropertyValue,
+  TitlePropertyValue,
+} from "@notionhq/client/build/src/api-types"
 import { formatISO, subDays } from "date-fns"
 
 import { config } from "dotenv"
@@ -10,14 +14,15 @@ config()
 const octokit = new Octokit({ auth: process.env["REPO_GITHUB_TOKEN"] })
 const notion = new Client({ auth: process.env["NOTION_KEY"] })
 
-const DATABASE_ID = process.env["NOTION_DATABASE_ID"]
-const PROPERTY_TITLE = process.env["PROPERTY_TITLE"]
-const PROPERTY_NO = process.env["PROPERTY_NO"]
-const PROPERTY_UPDATE_TARGET_DAYS = Number(process.env["PROPERTY_UPDATE_TARGET_DAYS"])
-const PROPERTY_STATUS = process.env["PROPERTY_STATUS"]
-const PROPERTY_VALUE_STATUS_DONE = process.env["PROPERTY_VALUE_STATUS_DONE"]
-const PROPERTY_UPDATED_TIME = process.env["PROPERTY_UPDATED_TIME"]
-
+const DATABASE_ID = process.env["NOTION_DATABASE_ID"]!
+const PROPERTY_TITLE = process.env["PROPERTY_TITLE"]!
+const PROPERTY_NO = process.env["PROPERTY_NO"]!
+const PROPERTY_UPDATE_TARGET_DAYS = Number(
+  process.env["PROPERTY_UPDATE_TARGET_DAYS"]
+)
+const PROPERTY_STATUS = process.env["PROPERTY_STATUS"]!
+const PROPERTY_VALUE_STATUS_DONE = process.env["PROPERTY_VALUE_STATUS_DONE"]!
+const PROPERTY_UPDATED_TIME = process.env["PROPERTY_UPDATED_TIME"]!
 
 type Page = {
   id: string
@@ -29,18 +34,25 @@ type Page = {
 
 async function updateGitHubIssues(tasks: Page[]) {
   for (const [index, task] of Object.entries(tasks)) {
-    const state = PROPERTY_VALUE_STATUS_DONE.split(',').includes(task.status) ? 'closed' : 'open'
+    const state = PROPERTY_VALUE_STATUS_DONE.split(",").includes(task.status)
+      ? "closed"
+      : "open"
 
     const updatedIssue = await octokit.rest.issues.update({
-      owner: process.env["REPO_GITHUB_OWNER"],
-      repo: process.env["REPO_GITHUB_REPO"],
+      owner: process.env["REPO_GITHUB_OWNER"] ?? "",
+      repo: process.env["REPO_GITHUB_REPO"] ?? "",
       issue_number: task.issue_number,
       title: task.title,
       state: state,
     })
 
     // console.log('updated', task.title, task.issue_number, state)
-    console.log('updated', updatedIssue.data.title, updatedIssue.data.number, updatedIssue.data.state)
+    console.log(
+      "updated",
+      updatedIssue.data.title,
+      updatedIssue.data.number,
+      updatedIssue.data.state
+    )
 
     // TODO: I'd really like to sleep here.(to avoid GitHub's api rate limit)
   }
@@ -50,7 +62,7 @@ async function getDoneTasksFromDatabase() {
   const doneTasks: Page[] = []
   const past_days = formatISO(subDays(new Date(), PROPERTY_UPDATE_TARGET_DAYS))
 
-  async function getPageOfTasks(cursor: string | null) {
+  async function getPageOfTasks(cursor: string) {
     const current_pages = await notion.databases.query({
       database_id: DATABASE_ID,
       filter: {
@@ -58,35 +70,35 @@ async function getDoneTasksFromDatabase() {
           {
             property: PROPERTY_UPDATED_TIME,
             date: {
-              after: past_days
+              after: past_days,
             },
           },
           {
             property: PROPERTY_TITLE,
             text: {
-              is_not_empty: true
-            }
+              is_not_empty: true,
+            },
           },
           {
             property: PROPERTY_STATUS,
             select: {
-              is_not_empty: true
-            }
+              is_not_empty: true,
+            },
           },
           {
             property: PROPERTY_NO,
             number: {
-              is_not_empty: true
-            }
-          }
+              is_not_empty: true,
+            },
+          },
         ],
       },
-      start_cursor: cursor
+      start_cursor: cursor,
     })
-    console.log('pages count: ', current_pages.results.length)
+    console.log("pages count: ", current_pages.results.length)
 
     for (const page of current_pages.results) {
-      if (page.object === 'page') {
+      if (page.object === "page") {
         // console.debug(page.properties)
         const title = page.properties[PROPERTY_TITLE] as TitlePropertyValue
         const status = page.properties[PROPERTY_STATUS] as SelectPropertyValue
@@ -94,17 +106,17 @@ async function getDoneTasksFromDatabase() {
         doneTasks.push({
           id: page.id,
           title: title.title[0].plain_text,
-          status: status.select.name,
+          status: status.select.name ?? "",
           issue_number: issue_number.number,
-          lastEditedTime: page.last_edited_time
+          lastEditedTime: page.last_edited_time,
         })
       }
     }
     if (current_pages.has_more) {
-      await getPageOfTasks(current_pages.next_cursor)
+      await getPageOfTasks(current_pages.next_cursor ?? "")
     }
   }
-  await getPageOfTasks(undefined)
+  await getPageOfTasks("")
   return doneTasks
 }
 
